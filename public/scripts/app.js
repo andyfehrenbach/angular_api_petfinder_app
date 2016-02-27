c32c0a4fd8c17c44f1392fb55f0e2e3c74d840cb
andyfehrenbach <andy.fehrenbach@gmail.com>
2016-02-27 16:39:57 -0600

    JS: getAnimalController will get a random animal using a variable inserted into the query string.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,14 +3,10 @@ var myApp = angular.module('myApp', ['ngRoute']);
 myApp.config(['$routeProvider', function($routeProvider) {
 
     $routeProvider
-        .when('/header', {
-            templateUrl: '/views/templates/header.html',
-            controller: 'getAnimalController'
-
-        })
 
         .when('/home', {
             templateUrl: '/views/templates/home.html',
+            controller: 'getAnimalController'
 
         })
 
--- public/scripts/controllers/get_animal_controller.js
+++ public/scripts/controllers/get_animal_controller.js
@@ -1,26 +1,32 @@
-myApp.controller('getAnimalController', ['$scope', 'DataFactory', function($scope, DataFactory) {
+myApp.controller('getAnimalController', ['$scope', '$http', function($scope, $http) {
 
-    $scope.getAnimal = function () {
-            console.log('get animal controller');
-    };
+    console.log('the controller is working');
+    $scope.data = {};
+    $scope.animal = 'horse';
+    console.log($scope.animal);
 
 
-    // $scope.dataFactory = DataFactory;
-    // $scope.message = 'People!';
-    // $scope.formName = '';
-    //
-    // if($scope.dataFactory.peopleData() === undefined) {
-    //     // initial load
-    //     $scope.dataFactory.retrieveData().then(function() {
-    //         $scope.people = $scope.dataFactory.peopleData();
-    //     });
-    // } else {
-    //     $scope.people = $scope.dataFactory.peopleData();
-    // }
-    //
-    // $scope.addPerson = function() {
-    //     $scope.dataFactory.addName($scope.formName);
-    //     $scope.formName = '';
-    // }
+    function getAnimal() {
+        // API key
+        var key = 'aeae1463b9ce72e46a418089c2b8c21f';
 
+        var baseURL = 'http://api.petfinder.com/';
+        var query = 'pet.getRandom';
+        query += '?key=' + key;
+        query += '&animal='+ $scope.animal;
+        query += '&output=basic';
+        query += '&format=json';
+
+        var request = baseURL + encodeURI(query) + '&callback=JSON_CALLBACK';
+        console.log(request);
+
+        $http.jsonp(request).then(
+            function(response) {
+                $scope.animal = response.data.petfinder.pet;
+                console.log($scope.animal);
+            }
+        );
+    }
+
+    getAnimal();
 }]);
